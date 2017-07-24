a9cd5aa7718a1f163d09d3375e6291172343da46
Modeste KOUADIO <modstk1@gmail.com>
2017-07-24 19:11:43 +0000

    Add reducer to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, combineReducers } from 'redux';
+
 import App from './components/App';
+
+const articlesReducer = (state = [], action) => {
+    switch(action.type){
+        case 'ADD_ARTICLE':
+            console.log('ADD_ARTICLE');
+            console.log('action', action);
+            action.payload.id = Date.now();
+            const newState =  [...state, action.payload];
+            return newState;
+        default:
+            return state;
+    }
+};
+
+const store = createStore(combineReducers({articles: articlesReducer}));
 
 ReactDOM.render(<App/>, document.getElementById('root'));
